1494dedd1d4aa1d137eea0de0ffc0aba8106293d
Mael-Kouassi <mael.kouassi.pro@gmail.com>
2025-05-20 17:12:00 +0200

    Add files via upload

---
+++ appwizard/ihm_twizy/Resource/Text/APPW_Language_0.c
@@ -0,0 +1,24 @@
+/*********************************************************************
+*                     SEGGER Microcontroller GmbH                    *
+*        Solutions for real time microcontroller applications        *
+**********************************************************************
+*                                                                    *
+*        (c) 1996 - 2025  SEGGER Microcontroller GmbH                *
+*                                                                    *
+*        Internet: www.segger.com    Support:  support@segger.com    *
+*                                                                    *
+**********************************************************************
+----------------------------------------------------------------------
+File        : APPW_Language_0.c
+Purpose     : Conversion into c-array.
+              Generated file do NOT edit!
+---------------------------END-OF-HEADER------------------------------
+*/
+
+#include "AppWizard.h"
+
+GUI_CONST_STORAGE unsigned char acAPPW_Language_0[28UL + 1] = {
+  0x56, 0x69, 0x74, 0x65, 0x73, 0x73, 0x65, 0x0A, 0x42, 0x61, 0x74, 0x74, 0x65, 0x72, 0x69, 0x65, 0x0A, 0x52, 0x61, 0x70, 0x70, 0x6F, 0x72, 0x74, 0x0A, 
+};
+
+/*************************** End of file ****************************/
