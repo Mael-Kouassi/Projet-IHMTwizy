c6f79f0ef054df7ced39ef7a304d12cbacb9129e
Mael-Kouassi <mael.kouassi.pro@gmail.com>
2025-05-20 17:06:00 +0200

    Code initial
    Interface très sommaire et non réactive

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,55 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'test' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f7xx.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5:Source:5.9.0 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/* Keil::Device:STM32Cube Framework:Classic:1.2.0 */
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+/* Keil::Device:STM32Cube HAL:Common:1.2.0 */
+#define RTE_DEVICE_HAL_COMMON
+/* Keil::Device:STM32Cube HAL:Cortex:1.2.0 */
+#define RTE_DEVICE_HAL_CORTEX
+/* Keil::Device:STM32Cube HAL:DMA2D:1.2.0 */
+#define RTE_DEVICE_HAL_DMA2D
+/* Keil::Device:STM32Cube HAL:DMA:1.2.0 */
+#define RTE_DEVICE_HAL_DMA
+/* Keil::Device:STM32Cube HAL:GPIO:1.2.0 */
+#define RTE_DEVICE_HAL_GPIO
+/* Keil::Device:STM32Cube HAL:I2C:1.2.0 */
+#define RTE_DEVICE_HAL_I2C
+/* Keil::Device:STM32Cube HAL:LTDC:1.2.0 */
+#define RTE_DEVICE_HAL_LTDC
+/* Keil::Device:STM32Cube HAL:PWR:1.2.0 */
+#define RTE_DEVICE_HAL_PWR
+/* Keil::Device:STM32Cube HAL:RCC:1.2.0 */
+#define RTE_DEVICE_HAL_RCC
+/* Keil::Device:STM32Cube HAL:SDRAM:1.2.0 */
+#define RTE_DEVICE_HAL_SDRAM
+/* Keil::Device:STM32Cube HAL:UART:1.2.0 */
+#define RTE_DEVICE_HAL_UART
+/* Keil::Device:Startup:1.2.0 */
+#define RTE_DEVICE_STARTUP_STM32F7XX    /* Device Startup for STM32F7 */
+/* Segger.MDK-Pro::Graphics:CORE:6.32.3 */
+#define RTE_Graphics_Core               /* Graphics Core */
+/* Segger.MDK-Pro::Graphics:Input Device:Touchscreen:6.32.3 */
+#define RTE_Graphics_Touchscreen        /* Graphics Input Device Touchscreen */
+
+
+#endif /* RTE_COMPONENTS_H */
